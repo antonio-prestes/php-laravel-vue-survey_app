c3689b7d752a4449b39258bd022fce63e4e8a533
antonio-prestes <diegoantoniodasp@hotmail.com>
2022-03-20 08:51:37 -0300

    01:31:00 - Survey Card with Tailwindcss

--- vue/src/components/PageComponent.vue
+++ vue/src/components/PageComponent.vue
@@ -1,16 +1,16 @@
 <template>
-    <header className="bg-white shadow">
-        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 v-if="title" className="text-3xl font-bold text-gray-900">
+    <header class="shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 v-if="title" class="text-3xl font-bold text-gray-900">
                 {{ title }}
             </h1>
             <slot name="header"></slot>
         </div>
     </header>
     <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             <!-- Replace with your content -->
-            <div className="px-4 sm:px-0">
+            <div class="px-4 sm:px-0">
                 <slot></slot>
             </div>
             <!-- /End replace -->
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -77,6 +77,82 @@ const tmpSurveys = [
                 data: {}
             }
         ]
+    },{
+
+        id: 102,
+        title: "Teste 444",
+        slug: "teste-123",
+        status: "draft",
+        image:
+            "https://images.unsplash.com/photo-1638913971789-667874197280?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+        description: "Lorem impsun loren impsun lorenimpsun lo renmipsun",
+        created_at: "2022-03-16 00:00:00",
+        updated_at: "2022-03-16 00:00:00",
+        expire_date: "2022-03-17 00:00:00",
+        questions: [
+            {
+                id: 1,
+                type: "select",
+                question: "From which country are you",
+                description: null,
+                data: {
+                    options: [
+                        {uuid: "afd3afa8-a595-11ec-b909-0242ac120002", text: "USA"},
+                        {uuid: "b8a490c0-a595-11ec-b909-0242ac120002", text: "Brazil"}
+                    ]
+                }
+            },
+            {
+                id: 2,
+                type: "checkbox",
+                question: "From which country are you",
+                description: null,
+                data: {
+                    options: [
+                        {uuid: "23d3afa8-a595-11ec-b909-0242ac120002", text: "USA"},
+                        {uuid: "32a490c0-a595-11ec-b909-0242ac120002", text: "Brazil"}
+                    ]
+                }
+            },
+            {
+                id: 3,
+                type: "checkbox",
+                question: "From which country are you",
+                description: null,
+                data: {
+                    options: [
+                        {uuid: "23d3afa8-a595-11ec-b909-0242ac120002", text: "USA"},
+                        {uuid: "32a490c0-a595-11ec-b909-0242ac120002", text: "Brazil"}
+                    ]
+                }
+            },
+            {
+                id: 4,
+                type: "radio",
+                question: "From which country are you",
+                description: null,
+                data: {
+                    options: [
+                        {uuid: "23d3afa8-a595-11ec-b909-0242ac120002", text: "USA"},
+                        {uuid: "32a490c0-a595-11ec-b909-0242ac120002", text: "Brazil"}
+                    ]
+                }
+            },
+            {
+                id: 5,
+                type: "text",
+                question: "From which country are you",
+                description: null,
+                data: {}
+            },
+            {
+                id: 6,
+                type: "textarea",
+                question: "From which country are you",
+                description: null,
+                data: {}
+            }
+        ]
     }
 ]
 
